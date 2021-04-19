147e40010f21d79eda9469e0b8b3c11498ea618d
Muhammad Apriandito A.S <m.apriandito@gmail.com>
2021-04-19 10:38:54 +0700

    Fix Code

--- src/001.R
+++ src/001.R
@@ -6,22 +6,24 @@ library(tidyverse)
 library(GA)
 
 # Create Data
-item = c('raincoat',
-         'pocket knife',
-         'mineral water',
-         'gloves',
-         'sleeping bag',
-         'tent',
-         'portable stove',
-         'canned food',
-         'snacks')
-weight = c(2,1,6,1,4,9,5,8,3)
-survival_point = c(5,3,15,5,6,18,8,20,8)
+item <- c(
+  "raincoat",
+  "pocket knife",
+  "mineral water",
+  "gloves",
+  "sleeping bag",
+  "tent",
+  "portable stove",
+  "canned food",
+  "snacks"
+)
+weight <- c(2, 1, 6, 1, 4, 9, 5, 8, 3)
+survival_point <- c(5, 3, 15, 5, 6, 18, 8, 20, 8)
 
-data = tibble(item,weight,survival_point)
+data <- tibble(item, weight, survival_point)
 
 # Create Chromosom Representation
-chromosomes = c(0,1,1,0,0,0,0,0,1)
+chromosomes <- c(0, 1, 1, 0, 0, 0, 0, 0, 1)
 
 # Check Item
 data[chromosomes == 1, ]
@@ -30,7 +32,7 @@ data[chromosomes == 1, ]
 chromosomes %*% data$survival_point
 
 # Set limit parameter
-max_weight = 25
+max_weight <- 25
 
 # Create fitness Function
 fitness <- function(x) {
@@ -45,18 +47,18 @@ fitness <- function(x) {
 }
 
 # Create Genetic Algorithm Model
-GA=ga(type ='binary',
-      fitness = fitness,
-      nBits=nrow(data),
-      maxiter=100,
-      popSize=50,
-      seed=1234,
-      keepBest=TRUE)
+GA <- ga(
+  type = "binary",
+  fitness = fitness,
+  nBits = nrow(data),
+  maxiter = 100,
+  popSize = 50,
+  seed = 1234,
+  keepBest = TRUE
+)
 
 # See Model Summary
 summary(GA)
 
 # Visualize Model
 plot(GA, legend = FALSE)
-
- 
