f7f0d243367e3184ae88472902f15915d3ca9949
Muhammad Apriandito A.S <m.apriandito@gmail.com>
2021-04-19 09:41:03 +0700

    Styling

--- src/001.R
+++ src/001.R
@@ -15,8 +15,8 @@ item = c('raincoat',
          'portable stove',
          'canned food',
          'snacks')
-weight=c(2,1,6,1,4,9,5,8,3)
-survival_point =c(5,3,15,5,6,18,8,20,8)
+weight = c(2,1,6,1,4,9,5,8,3)
+survival_point = c(5,3,15,5,6,18,8,20,8)
 
 data = tibble(item,weight,survival_point)
 
